11fff5665f8b72f40989668e7dbcaea42f84d240
Gowtham Jangiti <gowtham310703@gmail.com>
2024-10-12 17:31:51 +0530

    Assignment 1 Complete

--- js/index.js
+++ js/index.js
@@ -4,3 +4,27 @@ console.log(`The driver's name is ${hacker1}`);
 
 let hacker2 = "Jane";
 console.log(`The navigator's name is ${hacker2}`);
+
+// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
+
+// Iteration 3: Loops
+let driverNameSpaced = hacker1.toUpperCase().split('').join(' ');
+console.log(driverNameSpaced);
+
+let navigatorNameReversed = hacker2.split('').reverse().join('');
+console.log(navigatorNameReversed);
+
+if (hacker1.localeCompare(hacker2) < 0) {
+    console.log("The driver's name goes first.");
+} else if (hacker1.localeCompare(hacker2) > 0) {
+    console.log("Yo, the navigator goes first definitely.");
+} else {
+    console.log("What?! You both have the same name?");
+}
