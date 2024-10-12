373e7abd109fcf19fd340470c8261a7a197fc58b
Gowtham Jangiti <gowtham310703@gmail.com>
2024-10-12 17:24:04 +0530

    Assignment 1 Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log("I'm ready!");
+// Iteration 1: Names and Input
+let hacker1 = "John";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "Jane";
+console.log(`The navigator's name is ${hacker2}`);
